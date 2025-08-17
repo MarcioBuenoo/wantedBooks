c0a2b4ecb206e99088fbdf1b387c24e25903c8c7
MarcioBuenoo <marcinbueno01@hotmail.com>
2025-08-17 18:41:44 -0300

    update/bd.sql

--- bd.sql
+++ bd.sql
@@ -4,13 +4,11 @@ CREATE TABLE dbLivros (
     ISBN INT PRIMARY KEY NOT NULL,
     nomeLivro VARCHAR(255) NOT NULL,
     autor VARCHAR(255) NOT NULL,
-    dataPublicacao DATE NOT NULL,
-)
+    dataPublicacao DATE NOT NULL )
 
 CREATE TABLE dbUsuario (
     id_user INT PRIMARY KEY NOT NULL,
     nomeUser VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    senha PASSWORD NOT NULL,
-)
+    senha VARCHAR NOT NULL )
 
