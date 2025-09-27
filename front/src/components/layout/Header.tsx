b3ec4c4cc4db76a47ee9a7c59a28ad222077a97a
MarcioBuenoo <marcinbueno01@hotmail.com>
2025-09-27 14:05:16 -0300

    feat/ create Header component

---
+++ front/src/components/layout/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <a href="#">
+                WantedBooks
+            </a>
+            
+        </header>
+    )
+}
---
+++ front/src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+import Header from "../components/layout/Header";
+
+export default function HomePage () {
+    return (
+        <Header/>
+    )
+}
