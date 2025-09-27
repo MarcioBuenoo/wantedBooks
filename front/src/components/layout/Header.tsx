e247bb6de4a41359c9ad9e1d5ba1aafbef118aa4
MarcioBuenoo <marcinbueno01@hotmail.com>
2025-09-27 18:39:12 -0300

    feat/ remake Header component

--- front/src/components/layout/Header.tsx
+++ front/src/components/layout/Header.tsx
@@ -1,30 +1,14 @@
-// src/components/Header/Header.tsx
+import '../../styles/Header.css'
 
-import React, { useState } from 'react';
-import Sidebar from '../ui/Sidebar'; // Importamos o componente Sidebar
-import '../../styles/Header.css'; // CSS para o Header
-
-const Header: React.FC = () => {
-  const [isSidebarOpen, setSidebarOpen] = useState(false);
-
-  // Função para alternar o estado da sidebar
-  const toggleSidebar = () => {
-    setSidebarOpen(!isSidebarOpen);
-  };
-
-  return (
-    <>
-      <header className="app-header">
-        <div className="logo">
-          wantedBooks
-        </div>
-        <button onClick={toggleSidebar} className="menu-button">
-          ☰ {/* Ícone de menu (hambúrguer) */}
-        </button>
-      </header>
-      <Sidebar isOpen={isSidebarOpen} onClose={toggleSidebar} />
-    </>
-  );
-};
-
-export default Header;
+export default function Header() {
+  return(
+    <header className="Header-container">
+      <a href="" className='Header-titulo'>
+        wantedBooks
+      </a>
+      <button className='Header-button'>
+        ☰
+      </button>
+    </header>
+  )
+}
