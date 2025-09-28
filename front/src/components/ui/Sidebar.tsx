1e83cbb6db325c378ad9def1dfb9e2ea27b36f9b
MarcioBuenoo <marcinbueno01@hotmail.com>
2025-09-28 17:50:55 -0300

    feat/ fix sidebar component

--- front/src/components/ui/Sidebar.tsx
+++ front/src/components/ui/Sidebar.tsx
@@ -1,33 +1,37 @@
 import React from 'react';
-import '../../styles/Sidebar.css'; // Importaremos o nosso CSS aqui
+import '../../styles/Sidebar.css'; 
+
 
 interface SidebarProps {
-    isOpen: boolean;
-    onClose: () => void;
+  isOpen: boolean;      
+  onClose: () => void;  
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
-    const sidebarClassName = isOpen? "sidebar-open" : "sidebar";
 
-    return (
-        <div className={sidebarClassName}>
-            <div className="sidebar-overlay" onClick={onClose}></div>
+  const sidebarClassName = isOpen ? "sidebar open" : "sidebar";
 
-            <div className="sidebar-content">
-                <button onClick={onClose} className="close-button">
-                    &times;
-                </button>
+  return (
+    <div className={sidebarClassName}>
 
-                <h2>Menu</h2>
-                <ul>
-                    <li><a href="#inicio" onClick={onClose}>Início</a></li>
-                    <li><a href="#servicos" onClick={onClose}>Serviços</a></li>
-                    <li><a href="#sobre" onClick={onClose}>Sobre</a></li>
-                    <li><a href="#contato" onClick={onClose}>Contato</a></li>
-                </ul>
-            </div>
-        </div>
-    )
+      <div className="sidebar-overlay" onClick={onClose}></div>
+      
+    
+      <div className="sidebar-content">
+        <button onClick={onClose} className="close-button">
+          &times; 
+        </button>
+        
+        <h2>Menu</h2>
+        <ul>
+          <li><a href="#inicio" onClick={onClose}>Início</a></li>
+          <li><a href="#servicos" onClick={onClose}>Serviços</a></li>
+          <li><a href="#sobre" onClick={onClose}>Sobre</a></li>
+          <li><a href="#contato" onClick={onClose}>Contato</a></li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
